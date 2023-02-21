81a197c32f334bb5d93f1139b71d3270c2d202a6
Ben Botsford <ben.botsford@flatironschool.com>
2023-02-21 11:02:11 -0600

    remove create_engine

--- lib/models.py
+++ lib/models.py
@@ -1,8 +1,5 @@
-from sqlalchemy import create_engine
 from sqlalchemy import ForeignKey, Column, Integer, String
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
-
-engine = create_engine('sqlite:///one_to_many.db')
 
 Base = declarative_base()
