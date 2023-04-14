553f7126f33b641609c920001227aa943957cf02
Ben Botsford <ben.botsford@flatironschool.com>
2023-04-14 14:47:00 -0500

    add solution

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,33 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+
+    reviews = relationship('Review', backref=backref('game'), cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'Game(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'platform={self.platform})'
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
+    
+    game_id = Column(Integer(), ForeignKey('games.id'))
+
+    def __repr__(self):
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
